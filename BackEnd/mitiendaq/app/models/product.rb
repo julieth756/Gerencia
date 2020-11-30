fac1f211f2f41cfda40ba6b5c6cedce332daa13e
Julieth Cobos <jcobosg@unbosque.edu.co>
2020-11-29 21:33:57 -0500

    imagen

--- BackEnd/mitiendaq/app/models/product.rb
+++ BackEnd/mitiendaq/app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
     has_attached_file :image, styles: { medium: '200x200>', thumb: '48x48>' }, :default_url => "/images/:style/missing.png"
-    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+    # Validate content type
+    validates_attachment_content_type :image, content_type: /\Aimage/
+    # Validate filename
+    validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/]
+    # Explicitly do not validate
+    do_not_validate_attachment_file_type :image
 end
